bd08aec9b3259038a9646696d1f9bd6e8fbe6cdd
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-14 12:34:50 +0200

    updating named paramaters examples adding comments

--- _examples/named_parameters/main.go
+++ _examples/named_parameters/main.go
@@ -11,6 +11,7 @@ func main() {
 	// Match to /hello/iris
 	// Not match to /hello or /hello/ or /hello/iris/something
 	iris.Get("/hello/:name", func(c *iris.Context) {
+		// Retrieve the parameter name
 		name := c.Param("name")
 		c.Write("Hello " + name)
 	})
@@ -20,13 +21,15 @@ func main() {
 	// Not match to /profile/iris/friends,  /profile/iris/friends ,
 	// Not match to /profile/iris/friends/2/something
 	iris.Get("/profile/:fullname/friends/:friendID", func(c *iris.Context) {
-
+		// Retrieve the parameters fullname and friendID
 		fullname := c.Param("fullname")
 		friendID, err := c.ParamInt("friendID")
 		if err != nil {
+			// Do something with the error
 		}
 		c.HTML("<b> Hello </b>" + fullname + "<b> with friends ID </b>" + strconv.Itoa(friendID))
 	})
 
+	// Listen to port 8080 for example localhost:8080
 	iris.Listen(8080)
 }
