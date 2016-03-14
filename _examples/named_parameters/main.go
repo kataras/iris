78eb50ea14de6e6040333d67e6ef2b8b4fd63949
Spiros Gerokostas <spiros.gerokostas@gmail.com>
2016-03-14 12:32:18 +0200

    .

--- _examples/named_parameters/main.go
+++ _examples/named_parameters/main.go
@@ -1,12 +1,25 @@
 package main
 
-import "github.com/kataras/iris"
+import (
+	"strconv"
+
+	"github.com/kataras/iris"
+)
 
 func main() {
 
 	iris.Get("/hello/:name", func(c *iris.Context) {
 		name := c.Param("name")
 		c.Write("Hello " + name)
+	})
+
+	iris.Get("/profile/:fullname/friends/:friendID", func(c *iris.Context) {
+
+		fullname := c.Param("fullname")
+		friendID, err := c.ParamInt("friendID")
+		if err != nil {
+		}
+		c.HTML("<b> Hello </b>" + fullname + "<b> with friends ID </b>" + strconv.Itoa(friendID))
 	})
 
 	iris.Listen(8080)
