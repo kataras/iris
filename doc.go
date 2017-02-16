53f75b0f1d748cc7bc96fd8d2b963594a8074f9f
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-02-17 00:09:04 +0200

    Update readme

--- doc.go
+++ doc.go
@@ -19,9 +19,9 @@
 // SOFTWARE.
 
 /*
-Package iris is an efficient and well-designed toolbox with robust set of features to
+Package iris provides efficient and well-designed tools with robust set of features to
 create your own perfect high performance web application
-with unlimited portability using the Go Programming Language.
+with unlimited potentials and portability.
 
 Note: This package is under active development status.
 Each month a new version is releasing
