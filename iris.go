d6fb93b3de2f1ac07c0bb80797841b9d41bcc7d7
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-02-18 07:22:57 +0200

    Fix history big mistakes, point links to /v6 tag only https://github.com/kataras/iris/issues/606

--- iris.go
+++ iris.go
@@ -848,9 +848,9 @@ Edit your main .go source file to adapt one of these and restart your app.
 	i.e: lines (<---) were missing.
 	-------------------------------------------------------------------
 	import (
-		"github.com/kataras/iris"
-		"github.com/kataras/iris/adaptors/httprouter" // or gorillamux
-		"github.com/kataras/iris/adaptors/view" // <--- this line
+		"gopkg.in/kataras/iris.v6"
+		"gopkg.in/kataras/iris.v6/adaptors/httprouter" // or gorillamux
+		"gopkg.in/kataras/iris.v6/adaptors/view" // <--- this line
 	)
 
 	func main(){
