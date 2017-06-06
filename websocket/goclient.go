b7a68b576bb6ed620800160988959c5b5a2e3d4c
Joseph deBlaquiere <jadeblaquiere@yahoo.com>
2017-06-06 02:49:41 +0000

    native go client using iris websocket message protocol

---
+++ websocket/goclient.go
@@ -0,0 +1,276 @@
+// Copyright 2017 Joseph deBlaquiere. All rights reserved.
+// Copyright 2017 Gerasimos Maropoulos, ΓΜ. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package websocket
+
+import (
+	"bytes"
+	"crypto/tls"
+	"fmt"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+
+	gwebsocket "github.com/gorilla/websocket"
+)
+
+// Client presents a subset of iris.websocket.Connection interface to support
+// client-initiated connections using the Iris websocket message protocol
+type Client struct {
+	conn                     *gwebsocket.Conn
+	config                   Config
+	wchan                    chan []byte
+	pchan                    chan []byte
+	onDisconnectListeners    []DisconnectFunc
+	onErrorListeners         []ErrorFunc
+	onNativeMessageListeners []NativeMessageFunc
+	onEventListeners         map[string][]MessageFunc
+}
+
+// in order to ensure all read operations are within a single goroutine
+// readPump processes incoming messages and dispatches them to messageReceived
+func (c *Client) readPump() {
+	defer c.conn.Close()
+	c.conn.SetReadLimit(c.config.MaxMessageSize)
+	c.conn.SetReadDeadline(time.Now().Add(c.config.ReadTimeout))
+	c.conn.SetPongHandler(func(s string) error {
+		// fmt.Printf("received PONG from %s\n", c.conn.UnderlyingConn().RemoteAddr().String())
+		c.conn.SetReadDeadline(time.Now().Add(c.config.ReadTimeout))
+		return nil
+	})
+	c.conn.SetPingHandler(func(s string) error {
+		// fmt.Printf("received PING (%s) from %s\n", s, c.conn.UnderlyingConn().RemoteAddr().String())
+		c.conn.SetReadDeadline(time.Now().Add(c.config.ReadTimeout))
+		c.pchan <- []byte(s)
+		return nil
+	})
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			fmt.Println("panic @ ", time.Now().Format("2006-01-02 15:04:05.000000"))
+			panic(err)
+		}
+		c.conn.SetReadDeadline(time.Now().Add(c.config.ReadTimeout))
+
+		// fmt.Println("recv:", string(message), "@", time.Now().Format("2006-01-02 15:04:05.000000"))
+
+		go c.messageReceived(message)
+	}
+}
+
+// messageReceived comes straight from iris/adapters/websocket/connection.go
+// messageReceived checks the incoming message and fire the nativeMessage listeners or the event listeners (ws custom message)
+func (c *Client) messageReceived(data []byte) {
+
+	if bytes.HasPrefix(data, websocketMessagePrefixBytes) {
+		customData := string(data)
+		//it's a custom ws message
+		receivedEvt := getWebsocketCustomEvent(customData)
+		listeners := c.onEventListeners[receivedEvt]
+		if listeners == nil { // if not listeners for this event exit from here
+			return
+		}
+		customMessage, err := websocketMessageDeserialize(receivedEvt, customData)
+		if customMessage == nil || err != nil {
+			return
+		}
+
+		for i := range listeners {
+			if fn, ok := listeners[i].(func()); ok { // its a simple func(){} callback
+				fn()
+			} else if fnString, ok := listeners[i].(func(string)); ok {
+
+				if msgString, is := customMessage.(string); is {
+					fnString(msgString)
+				} else if msgInt, is := customMessage.(int); is {
+					// here if server side waiting for string but client side sent an int, just convert this int to a string
+					fnString(strconv.Itoa(msgInt))
+				}
+
+			} else if fnInt, ok := listeners[i].(func(int)); ok {
+				fnInt(customMessage.(int))
+			} else if fnBool, ok := listeners[i].(func(bool)); ok {
+				fnBool(customMessage.(bool))
+			} else if fnBytes, ok := listeners[i].(func([]byte)); ok {
+				fnBytes(customMessage.([]byte))
+			} else {
+				listeners[i].(func(interface{}))(customMessage)
+			}
+
+		}
+	} else {
+		// it's native websocket message
+		for i := range c.onNativeMessageListeners {
+			c.onNativeMessageListeners[i](data)
+		}
+	}
+
+}
+
+// In order to ensure all write operations are within a single goroutine
+// writePump handles write operations to the socket serially from channels
+func (c *Client) writePump() {
+	pingtimer := time.NewTicker(c.config.PingPeriod)
+	defer c.conn.Close()
+	c.conn.SetWriteDeadline(time.Now().Add(c.config.WriteTimeout))
+	for {
+		select {
+		case wmsg := <-c.wchan:
+			// fmt.Printf("WP: writing %s\n", string(wmsg))
+			w, err := c.conn.NextWriter(gwebsocket.TextMessage)
+			if err != nil {
+				fmt.Println("error getting NextWriter")
+				return
+			}
+			w.Write(wmsg)
+
+			if err := w.Close(); err != nil {
+				fmt.Println("error closing NextWriter")
+				return
+			}
+
+		case pmsg := <-c.pchan:
+			c.conn.SetWriteDeadline(time.Now().Add(c.config.WriteTimeout))
+			// fmt.Printf("sending PONG to %s\n", c.conn.UnderlyingConn().RemoteAddr().String())
+			if err := c.conn.WriteControl(gwebsocket.PongMessage, pmsg, time.Now().Add(c.config.WriteTimeout)); err != nil {
+				return
+			}
+
+		case <-pingtimer.C:
+			c.conn.SetWriteDeadline(time.Now().Add(c.config.WriteTimeout))
+			// fmt.Printf("sending PING to %s\n", c.conn.UnderlyingConn().RemoteAddr().String())
+			if err := c.conn.WriteControl(gwebsocket.PingMessage, []byte{}, time.Now().Add(c.config.WriteTimeout)); err != nil {
+				return
+			}
+		}
+	}
+}
+
+// EmitMessage sends a native (raw) message to the socket. The server will
+// receive this message using the handler specified with OnMessage()
+func (c *Client) EmitMessage(nativeMessage []byte) error {
+	c.wchan <- nativeMessage
+	return nil
+}
+
+// Emit sends a message to a particular event queue. The server will receive
+// these messages via the handler specified using On()
+func (c *Client) Emit(event string, data interface{}) error {
+	message, err := websocketMessageSerialize(event, data)
+	if err != nil {
+		return err
+	}
+	// fmt.Printf("Message %s\n", string(message))
+	c.wchan <- []byte(message)
+	return nil
+}
+
+//func (c *Client) OnDisconnect(f DisconnectFunc) {
+//
+//}
+
+//func (c *Client) OnError(f ErrorFunc) {
+//
+//}
+
+// OnMessage designates a listener callback function for raw messages. If
+// multiple callback functions are specified, all will be called for each
+// message
+func (c *Client) OnMessage(f NativeMessageFunc) {
+	c.onNativeMessageListeners = append(c.onNativeMessageListeners, f)
+}
+
+// On designates a listener callback for a specific event tag.  If multiple
+// callback functions are specified, all will be called for each message
+func (c *Client) On(event string, f MessageFunc) {
+	if c.onEventListeners[event] == nil {
+		c.onEventListeners[event] = make([]MessageFunc, 0)
+	}
+
+	c.onEventListeners[event] = append(c.onEventListeners[event], f)
+}
+
+//func (c *Client) Disconnect() error {
+//	return nil
+//}
+
+// WSDialer here is a shameless wrapper around gorilla.websocket.Dialer
+// which returns a wsclient.Client instead of the gorilla Connection on Dial()
+type WSDialer struct {
+	// NetDial specifies the dial function for creating TCP connections. If
+	// NetDial is nil, net.Dial is used.
+	NetDial func(network, addr string) (net.Conn, error)
+
+	// Proxy specifies a function to return a proxy for a given
+	// Request. If the function returns a non-nil error, the
+	// request is aborted with the provided error.
+	// If Proxy is nil or returns a nil *URL, no proxy is used.
+	Proxy func(*http.Request) (*url.URL, error)
+
+	// TLSClientConfig specifies the TLS configuration to use with tls.Client.
+	// If nil, the default configuration is used.
+	TLSClientConfig *tls.Config
+
+	// HandshakeTimeout specifies the duration for the handshake to complete.
+	HandshakeTimeout time.Duration
+
+	// ReadBufferSize and WriteBufferSize specify I/O buffer sizes. If a buffer
+	// size is zero, then a useful default size is used. The I/O buffer sizes
+	// do not limit the size of the messages that can be sent or received.
+	ReadBufferSize, WriteBufferSize int
+
+	// Subprotocols specifies the client's requested subprotocols.
+	Subprotocols []string
+
+	// EnableCompression specifies if the client should attempt to negotiate
+	// per message compression (RFC 7692). Setting this value to true does not
+	// guarantee that compression will be supported. Currently only "no context
+	// takeover" modes are supported.
+	EnableCompression bool
+
+	// Jar specifies the cookie jar.
+	// If Jar is nil, cookies are not sent in requests and ignored
+	// in responses.
+	Jar http.CookieJar
+
+	dialer *gwebsocket.Dialer
+}
+
+// Dial initiates a connection to a remote Iris server websocket listener
+// using the gorilla websocket Dialer and returns a Client connection
+// which can be used to emit and handle messages
+func (wsd *WSDialer) Dial(urlStr string, requestHeader http.Header, config Config) (*Client, *http.Response, error) {
+	if wsd.dialer == nil {
+		wsd.dialer = new(gwebsocket.Dialer)
+	}
+	wsd.dialer.NetDial = wsd.NetDial
+	wsd.dialer.Proxy = wsd.Proxy
+	wsd.dialer.TLSClientConfig = wsd.TLSClientConfig
+	wsd.dialer.HandshakeTimeout = wsd.HandshakeTimeout
+	wsd.dialer.ReadBufferSize = wsd.ReadBufferSize
+	wsd.dialer.WriteBufferSize = wsd.WriteBufferSize
+	wsd.dialer.Subprotocols = wsd.Subprotocols
+	wsd.dialer.EnableCompression = wsd.EnableCompression
+	wsd.dialer.Jar = wsd.Jar
+	conn, response, err := wsd.dialer.Dial(urlStr, requestHeader)
+	if err != nil {
+		return nil, response, err
+	}
+	c := new(Client)
+	c.conn = conn
+	c.config = config
+	c.config.Validate()
+	c.wchan = make(chan []byte)
+	c.pchan = make(chan []byte)
+	c.onEventListeners = make(map[string][]MessageFunc)
+	c.config.Validate()
+
+	go c.writePump()
+	go c.readPump()
+
+	return c, response, nil
+}
---
+++ websocket/goclient_test.go
@@ -0,0 +1,288 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2017, Joseph deBlaquiere <jadeblaquiere@yahoo.com>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+package websocket
+
+import (
+	stdContext "context"
+	"encoding/binary"
+	// "encoding/hex"
+	"fmt"
+	"net/http"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/kataras/iris"
+	"github.com/kataras/iris/context"
+	"github.com/kataras/iris/core/host"
+	"github.com/kataras/iris/view"
+	//"github.com/kataras/iris/websocket"
+)
+
+// test server model used to test client code
+
+type indexResponse struct {
+	RequestIP string `json:"request_ip"`
+	Time      int64  `json:"unix_time"`
+}
+
+type wsClient struct {
+	con Connection
+	wss *wsServer
+}
+
+func (wsc *wsClient) echoRawMessage(message []byte) {
+	// fmt.Println("recv :", string(message))
+	wsc.con.EmitMessage(message)
+}
+
+func (wsc *wsClient) echoString(message string) {
+	// fmt.Println("recv :", message)
+	wsc.con.Emit("echo_reply", message)
+}
+
+func (wsc *wsClient) lenString(message string) {
+	// fmt.Println("recv :", message)
+	wsc.con.Emit("len_reply", len(message))
+}
+
+func (wsc *wsClient) disconnect() {
+	fmt.Println("client disconnect @ ", time.Now().Format("2006-01-02 15:04:05.000000"))
+}
+
+type wsServer struct {
+	clients   []*wsClient
+	listMutex sync.Mutex
+	app       *iris.Application
+	srv       *http.Server
+	super     *host.Supervisor
+	ws        Server
+}
+
+func (wss *wsServer) connect(con Connection) {
+	wss.listMutex.Lock()
+	defer wss.listMutex.Unlock()
+
+	c := &wsClient{con: con, wss: wss}
+	wss.clients = append(wss.clients, c)
+
+	// fmt.Printf("Connect # active clients : %d\n", len(wss.clients))
+
+	con.OnMessage(c.echoRawMessage)
+
+	con.On("echo", c.echoString)
+
+	con.On("len", c.lenString)
+
+	con.OnDisconnect(c.disconnect)
+}
+
+func (wss *wsServer) disconnect(wsc *wsClient) {
+	wss.listMutex.Lock()
+	defer wss.listMutex.Unlock()
+
+	l := len(wss.clients)
+
+	if l == 0 {
+		panic("WSS:trying to delete client from empty list")
+	}
+
+	for p, v := range wss.clients {
+		if v == wsc {
+			wss.clients[p] = wss.clients[l-1]
+			wss.clients = wss.clients[:l-1]
+
+			fmt.Printf("Disconnect # active clients : %d\n", len(wss.clients))
+
+			return
+		}
+	}
+	panic("WSS:trying to delete client not in list")
+}
+
+func (wss *wsServer) index(ctx context.Context) {
+	t := time.Now().Unix()
+	ctx.StatusCode(iris.StatusOK)
+	ctx.JSON(indexResponse{RequestIP: ctx.RemoteAddr(), Time: t})
+}
+
+func (wss *wsServer) startup() {
+	wss.app = iris.New()
+	wss.app.AttachView(view.HTML("./templates", ".html"))
+	wss.app.Get("/", wss.index)
+	// create our echo websocket server
+	ws := New(Config{
+		ReadTimeout:     60 * time.Second,
+		WriteTimeout:    60 * time.Second,
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		BinaryMessages:  true,
+		Endpoint:        "/echo",
+	})
+
+	ws.OnConnection(wss.connect)
+
+	// Attach the websocket server.
+	ws.Attach(wss.app)
+
+	wss.srv = &http.Server{Addr: ":8080"}
+	wss.super = host.New(wss.srv)
+
+	go wss.app.Run(iris.Server(wss.srv), iris.WithoutBanner)
+}
+
+func (wss *wsServer) shutdown() {
+	ctx, _ := stdContext.WithTimeout(stdContext.Background(), 5*time.Second)
+	wss.super.Shutdown(ctx)
+}
+
+func TestConnectAndWait(t *testing.T) {
+	var wss wsServer
+	wss.startup()
+	time.Sleep(1 * time.Second)
+	d := new(WSDialer)
+	client, _, err := d.Dial("ws://127.0.0.1:8080/echo", nil, Config{
+		ReadTimeout:     60 * time.Second,
+		WriteTimeout:    60 * time.Second,
+		PingPeriod:      9 * 6 * time.Second,
+		PongTimeout:     60 * time.Second,
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		BinaryMessages:  true,
+	})
+	if err != nil {
+		fmt.Println("Dialer error:", err)
+		t.Fail()
+	}
+	if client == nil {
+		fmt.Println("Dialer returned nil client")
+		t.Fail()
+	} else {
+		// the wait here is longer than the Timeout, so the server will disconnect if
+		// ping/pong messages are not correctly triggered
+		for i := 0; i < 65; i++ {
+			// fmt.Printf("(sleeping) %s\n", time.Now().Format("2006-01-02 15:04:05.000000"))
+			time.Sleep(1 * time.Second)
+		}
+		got_reply := false
+		// fmt.Println("Dial complete")
+		time.Sleep(1 * time.Second)
+		client.On("echo_reply", func(s string) {
+			// fmt.Println("client echo_reply", s)
+			got_reply = true
+		})
+		// fmt.Println("ON complete")
+		time.Sleep(1 * time.Second)
+		client.Emit("echo", "hello")
+		// fmt.Println("Emit complete")
+		time.Sleep(1 * time.Second)
+		if !got_reply {
+			fmt.Println("No echo response")
+			t.Fail()
+		}
+	}
+	wss.shutdown()
+}
+
+func TestMixedMessages(t *testing.T) {
+	var wss wsServer
+	wss.startup()
+	time.Sleep(1 * time.Second)
+	d := new(WSDialer)
+	client, _, err := d.Dial("ws://127.0.0.1:8080/echo", nil, Config{
+		ReadTimeout:     60 * time.Second,
+		WriteTimeout:    60 * time.Second,
+		PingPeriod:      9 * 6 * time.Second,
+		PongTimeout:     60 * time.Second,
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+		BinaryMessages:  true,
+	})
+	if err != nil {
+		fmt.Println("Dialer error:", err)
+		t.Fail()
+	}
+	if client == nil {
+		fmt.Println("Dialer returned nil client")
+		t.Fail()
+	} else {
+		cycles := int(100)
+		echo_count := int(0)
+		len_count := int(0)
+		raw_count := int(0)
+		// fmt.Println("Dial complete")
+		time.Sleep(1 * time.Second)
+		client.On("echo_reply", func(s string) {
+			//fmt.Println("client echo_reply", s)
+			echo_count += 1
+		})
+		client.On("len_reply", func(i int) {
+			// fmt.Printf("client len_reply %d\n", i)
+			len_count += 1
+		})
+		client.OnMessage(func(b []byte) {
+			// fmt.Println("client raw_reply", hex.EncodeToString(b))
+			raw_count += 1
+		})
+		// fmt.Println("ON complete")
+		time.Sleep(1 * time.Second)
+		go func() {
+			for i := 0; i < cycles; i++ {
+				s := fmt.Sprintf("hello %d", i)
+				client.Emit("echo", s)
+			}
+		}()
+		go func() {
+			for i := 0; i < cycles; i++ {
+				s := make([]byte, i, i)
+				for j := 0; j < i; j++ {
+					s[j] = byte('a')
+				}
+				client.Emit("len", string(s))
+			}
+		}()
+		go func() {
+			for i := 0; i < cycles; i++ {
+				bb := make([]byte, 8)
+				binary.BigEndian.PutUint64(bb, uint64(i))
+				client.EmitMessage(bb)
+			}
+		}()
+		// fmt.Println("Emit complete")
+		time.Sleep(5 * time.Second)
+		// fmt.Printf("echo, len, raw = %d, %d, %d\n", echo_count, len_count, raw_count)
+		if echo_count != cycles {
+			fmt.Printf("echo count mismatch, %d != %d\n", echo_count, cycles)
+			t.Fail()
+		}
+		if len_count != cycles {
+			fmt.Printf("len count mismatch, %d != %d\n", len_count, cycles)
+			t.Fail()
+		}
+		if raw_count != cycles {
+			fmt.Printf("echo count mismatch, %d != %d\n", raw_count, cycles)
+			t.Fail()
+		}
+	}
+	wss.shutdown()
+}
