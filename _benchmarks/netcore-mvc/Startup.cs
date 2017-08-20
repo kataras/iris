89d0d5c5de87631de9e4e766335b738d02c838d6
kataras <kataras2006@hotmail.com>
2017-08-20 06:53:15 +0300

    Update the benchmarks results as Scott Hanselman‏ and Josh Clark noted.
    No such a difference (before: 8.61MB/s now: 8.91MB/s).
    
    I think we can't do much more, I already removed the heavy parts on the first run... but still I'm open for any suggestions. Thank you for the feedback!
    
    
    Former-commit-id: 0b0c8ba40c8380b530b6c77a5ee75ecfb4357f16

--- _benchmarks/netcore-mvc/Startup.cs
+++ _benchmarks/netcore-mvc/Startup.cs
@@ -23,7 +23,7 @@ namespace netcore_mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvcCore();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
