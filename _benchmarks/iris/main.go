40f79c1f19e4993f9631c166274613eb52387ec6
kataras <kataras2006@hotmail.com>
2017-08-21 16:17:32 +0300

    Kestrel vs Iris new benchmarks, as you asked for :+1:
    Former-commit-id: d8979e55679bbb586dd79a81a43e1c29082ad9c2

--- _benchmarks/iris/main.go
+++ _benchmarks/iris/main.go
@@ -7,22 +7,8 @@ import (
 
 func main() {
 	app := iris.New()
-	// These handlers are serving the same routes as
-	// `ValuesController`s of netcore-mvc and iris-mvc applications do.
-	app.Get("/api/values/{id}", getHandler)
-	app.Put("/api/values/{id}", putHandler)
-	app.Delete("/api/values/{id}", delHandler)
+	app.Get("/api/values/{id}", func(ctx context.Context) {
+		ctx.WriteString("value")
+	})
 	app.Run(iris.Addr(":5000"))
 }
-
-// getHandler handles "GET" requests to "api/values/{id}".
-func getHandler(ctx context.Context) {
-	// id,_ := vc.Params.GetInt("id")
-	ctx.WriteString("value")
-}
-
-// putHandler handles "PUT" requests to "api/values/{id}".
-func putHandler(ctx context.Context) {}
-
-// delHandler handles "DELETE" requests to "api/values/{id}".
-func delHandler(ctx context.Context) {}
