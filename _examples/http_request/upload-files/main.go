5758f2cd781c944cfc0f3d7c0cc0ae643e9114e0
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-10-03 05:14:53 +0300

    Add tutorial for DropzoneJS. Stay tuned for two-parts article series on https://medium.com/@kataras and https://dev.to/@kataras

--- _examples/http_request/upload-files/main.go
+++ _examples/http_request/upload-files/main.go
@@ -16,16 +16,16 @@ func main() {
 
 	app.RegisterView(iris.HTML("./templates", ".html"))
 
-	// Serve the form.html to the user
+	// Serve the upload_form.html to the client.
 	app.Get("/upload", func(ctx iris.Context) {
-		//create a token (optionally)
+		// create a token (optionally).
 
 		now := time.Now().Unix()
 		h := md5.New()
 		io.WriteString(h, strconv.FormatInt(now, 10))
 		token := fmt.Sprintf("%x", h.Sum(nil))
 
-		// render the form with the token for any use you like
+		// render the form with the token for any use you'd like.
 		ctx.ViewData("", token)
 		ctx.View("upload_form.html")
 	})
@@ -34,7 +34,7 @@ func main() {
 	app.Post("/upload", iris.LimitRequestBodySize(10<<20),
 		func(ctx iris.Context) {
 			// or use ctx.SetMaxRequestBodySize(10 << 20)
-			//to limit the uploaded file(s) size.
+			// to limit the uploaded file(s) size.
 
 			// Get the file from the request
 			file, info, err := ctx.FormFile("uploadfile")
