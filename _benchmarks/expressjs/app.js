26b91c4e10e833067a11b79d32a389c69ed3cd51
Quentin Homareau <quentin.homareau@gmail.com>
2017-11-19 13:02:48 +0100

    Fix NodeJS benchmark

--- _benchmarks/expressjs/app.js
+++ _benchmarks/expressjs/app.js
@@ -1,14 +1,23 @@
 process.env.NODE_ENV = 'production';
 
-const express = require('express');
-const app = express();
+const express      = require('express');
+const createWorker = require('throng')
 
-app.get('/api/values/:id', function (req, res) {
-  res.send('value');
-});
 
-app.listen(5000, function () {
-  console.log(
-    'Now listening on: http://localhost:5000\nApplication started. Press CTRL+C to shut down.'
-  )
-});
+createWorker(createWebServer)
+
+function createWebServer()
+{
+  const app = express();
+
+  app.get('/api/values/:id', function (req, res) {
+    res.send('value');
+  });
+
+  app.listen(5000, function () {
+    console.log(
+      'Now listening on: http://localhost:5000\nApplication started. Press CTRL+C to shut down.'
+    )
+  });
+  
+}
