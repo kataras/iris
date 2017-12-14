8d4721a5a855ebce0d53621b939886dac6f63078
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-12-14 05:56:23 +0200

    start of the vue + mvc example and a simple session binding

---
+++ mvc2/session_binder.go
@@ -0,0 +1,17 @@
+package mvc2
+
+import (
+	"github.com/kataras/iris/context"
+	"github.com/kataras/iris/sessions"
+)
+
+// Session -> TODO: think of move all bindings to
+// a different folder like "bindings"
+// so it will be used as .Bind(bindings.Session(manager))
+// or let it here but change the rest of the binding names as well
+// because they are not "binders", their result are binders to be percise.
+func Session(sess *sessions.Sessions) func(context.Context) *sessions.Session {
+	return func(ctx context.Context) *sessions.Session {
+		return sess.Start(ctx)
+	}
+}
