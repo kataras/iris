45e0c6f9425d67dc2fe8421b8439421c4e295160
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-12-20 18:10:57 +0200

    fix typo

--- _examples/mvc/singleton/main.go
+++ _examples/mvc/singleton/main.go
@@ -21,7 +21,7 @@ type globalVisitorsController struct {
 	// all clients share the same controller instance instead.
 	// Note that any controller's methods
 	// are per-client, but the struct's field can be shared across multiple clients if the structure
-	// does not have any dynamic struct field depenendies that depend on the iris.Context
+	// does not have any dynamic struct field dependencies that depend on the iris.Context
 	// and ALL field's values are NOT zero, at this case we use uint64 which it's no zero (even if we didn't set it
 	// manually ease-of-understand reasons) because it's a value of &{0}.
 	// All the above declares a Singleton, note that you don't have to write a single line of code to do this, Iris is smart enough.
