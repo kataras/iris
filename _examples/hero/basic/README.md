650cd6c294c623ad3a8a2ba7df1c5ddc62f72c69
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2017-12-27 03:17:06 +0200

    add images for hero basic examples to be easier to understand
    Former-commit-id: 012ec70fc7761e899f4114f956839a6adce7c21a

---
+++ _examples/hero/basic/README.md
@@ -0,0 +1,13 @@
+# hero: basic
+
+## 1. Path Parameters - Built'n Dependencies
+
+![](https://github.com/kataras/explore/raw/master/iris/hero/hero-1-monokai.png)
+
+## 2. Services - Static Dependencies
+
+![](https://github.com/kataras/explore/raw/master/iris/hero/hero-2-monokai.png)
+
+## References
+
+- [explore](https://github.com/kataras/explore)
---
+++ _examples/hero/basic/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"github.com/kataras/iris"
+
+	"github.com/kataras/iris/hero"
+)
+
+func main() {
+
+	app := iris.New()
+
+	// 1
+	helloHandler := hero.Handler(hello)
+	app.Get("/{to:string}", helloHandler)
+
+	// 2
+	hero.Register(&myTestService{
+		prefix: "Service: Hello",
+	})
+
+	helloServiceHandler := hero.Handler(helloService)
+	app.Get("/service/{to:string}", helloServiceHandler)
+
+	// http://localhost:8080/your_name
+	// http://localhost:8080/service/your_name
+	app.Run(iris.Addr(":8080"))
+}
+
+func hello(to string) string {
+	return "Hello " + to
+}
+
+type Service interface {
+	SayHello(to string) string
+}
+
+type myTestService struct {
+	prefix string
+}
+
+func (s *myTestService) SayHello(to string) string {
+	return s.prefix + " " + to
+}
+
+func helloService(to string, service Service) string {
+	return service.SayHello(to)
+}
