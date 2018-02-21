7a29519c5aa6eeb90660088952417da3e983afbd
ZaniaDevelopper <devteam@zania.group>
2018-02-21 14:48:09 +0300

    Fixes bad copy/paste
    Former-commit-id: 437b60bc6b37d8a755198fb97809c61b9c18004e

--- _examples/routing/fallback-handlers/main.go
+++ _examples/routing/fallback-handlers/main.go
@@ -11,11 +11,11 @@ func main() {
 	// will handle *all* expect DELETE requests, even if there is no routes
 	app.Get("/action/{p}", h)
 
-	app.Run(iris.Addr(":8080"), ctx.Method(), ctx.Path(), iris.WithoutServerError(iris.ErrServerClosed))
+	app.Run(iris.Addr(":8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
 
 func h(ctx iris.Context) {
-	ctx.Writef("[%s] %s : Parameter = `%s`", ctx.Params().Get("p"))
+	ctx.Writef("[%s] %s : Parameter = `%s`", ctx.Method(), ctx.Path(), ctx.Params().Get("p"))
 }
 
 func fallbackHandler(ctx iris.Context) {
