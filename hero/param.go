289b79ea921276113bd56d25c3f08e84fee553fb
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2018-08-23 06:33:54 +0300

    hero support for the new uint64

--- hero/param.go
+++ hero/param.go
@@ -46,6 +46,13 @@ func resolveParam(currentParamIndex int, typ reflect.Type) (reflect.Value, bool)
 
 			return v
 		}
+	case reflect.Uint64:
+		fn = func(ctx context.Context) uint64 {
+			entry, _ := ctx.Params().GetEntryAt(currentParamIndex)
+			v, _ := entry.Uint64Default(0)
+
+			return v
+		}
 	case reflect.Bool:
 		fn = func(ctx context.Context) bool {
 			entry, _ := ctx.Params().GetEntryAt(currentParamIndex)
