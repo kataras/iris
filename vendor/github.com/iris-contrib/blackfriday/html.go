342b88c42530bb54f420aaeff334d6ac88b6d08b
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2018-11-11 00:41:55 +0200

    move the blackfriday lib to the iris-contrib for the stability to avoid issues like: https://github.com/kataras/iris/issues/1132

--- vendor/github.com/iris-contrib/blackfriday/html.go
+++ vendor/github.com/iris-contrib/blackfriday/html.go
@@ -1,7 +1,3 @@
-//
-// Blackfriday Markdown Processor
-// Available at http://github.com/russross/blackfriday
-//
 // Copyright © 2011 Russ Ross <russ@russross.com>.
 // Distributed under the Simplified BSD License.
 // See README.md for details.
@@ -35,6 +31,7 @@ const (
 	Safelink                                      // Only link to trusted protocols
 	NofollowLinks                                 // Only link with rel="nofollow"
 	NoreferrerLinks                               // Only link with rel="noreferrer"
+	NoopenerLinks                                 // Only link with rel="noopener"
 	HrefTargetBlank                               // Add a blank target
 	CompletePage                                  // Generate a complete HTML page
 	UseXHTML                                      // Generate XHTML output instead of HTML
@@ -87,6 +84,10 @@ type HTMLRendererParameters struct {
 	HeadingIDPrefix string
 	// If set, add this text to the back of each Heading ID, to ensure uniqueness.
 	HeadingIDSuffix string
+	// Increase heading levels: if the offset is 1, <h1> becomes <h2> etc.
+	// Negative offset is also valid.
+	// Resulting levels are clipped between 1 and 6.
+	HeadingLevelOffset int
 
 	Title string // Document title (used if CompletePage is set)
 	CSS   string // Optional CSS file URL (used if CompletePage is set)
@@ -282,6 +283,9 @@ func appendLinkAttrs(attrs []string, flags HTMLFlags, link []byte) []string {
 	if flags&NoreferrerLinks != 0 {
 		val = append(val, "noreferrer")
 	}
+	if flags&NoopenerLinks != 0 {
+		val = append(val, "noopener")
+	}
 	if flags&HrefTargetBlank != 0 {
 		attrs = append(attrs, "target=\"_blank\"")
 	}
@@ -331,7 +335,7 @@ func (r *HTMLRenderer) tag(w io.Writer, name []byte, attrs []string) {
 
 func footnoteRef(prefix string, node *Node) []byte {
 	urlFrag := prefix + string(slugify(node.Destination))
-	anchor := fmt.Sprintf(`<a rel="footnote" href="#fn:%s">%d</a>`, urlFrag, node.NoteID)
+	anchor := fmt.Sprintf(`<a href="#fn:%s">%d</a>`, urlFrag, node.NoteID)
 	return []byte(fmt.Sprintf(`<sup class="footnote-ref" id="fnref:%s">%s</sup>`, urlFrag, anchor))
 }
 
@@ -460,9 +464,10 @@ var (
 )
 
 func headingTagsFromLevel(level int) ([]byte, []byte) {
-	switch level {
-	case 1:
+	if level <= 1 {
 		return h1Tag, h1CloseTag
+	}
+	switch level {
 	case 2:
 		return h2Tag, h2CloseTag
 	case 3:
@@ -471,9 +476,8 @@ func headingTagsFromLevel(level int) ([]byte, []byte) {
 		return h4Tag, h4CloseTag
 	case 5:
 		return h5Tag, h5CloseTag
-	default:
-		return h6Tag, h6CloseTag
 	}
+	return h6Tag, h6CloseTag
 }
 
 func (r *HTMLRenderer) outHRTag(w io.Writer) {
@@ -651,7 +655,8 @@ func (r *HTMLRenderer) RenderNode(w io.Writer, node *Node, entering bool) WalkSt
 		r.out(w, node.Literal)
 		r.cr(w)
 	case Heading:
-		openTag, closeTag := headingTagsFromLevel(node.Level)
+		headingLevel := r.HTMLRendererParameters.HeadingLevelOffset + node.Level
+		openTag, closeTag := headingTagsFromLevel(headingLevel)
 		if entering {
 			if node.IsTitleblock {
 				attrs = append(attrs, `class="title"`)
