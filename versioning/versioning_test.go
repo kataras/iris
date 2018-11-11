83f11ed58b275182d8843da39816b0cafe8f07ef
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2018-11-11 02:18:19 +0200

    add versioning/README.md

--- versioning/versioning_test.go
+++ versioning/versioning_test.go
@@ -23,6 +23,14 @@ func sendHandler(contents string) iris.Handler {
 	}
 }
 
+func TestIf(t *testing.T) {
+	if expected, got := true, versioning.If("1.0", ">=1"); expected != got {
+		t.Fatalf("expected %s to be %s", "1.0", ">= 1")
+	}
+	if expected, got := true, versioning.If("1.2.3", "> 1.2"); expected != got {
+		t.Fatalf("expected %s to be %s", "1.2.3", "> 1.2")
+	}
+}
 func TestNewMatcher(t *testing.T) {
 	app := iris.New()
 
@@ -32,6 +40,17 @@ func TestNewMatcher(t *testing.T) {
 		">= 2, < 3":         sendHandler(v2Response),
 		versioning.NotFound: notFoundHandler,
 	}))
+
+	// middleware as usual.
+	myMiddleware := func(ctx iris.Context) {
+		ctx.Header("X-Custom", "something")
+		ctx.Next()
+	}
+	myVersions := versioning.Map{
+		"1.0": sendHandler(v10Response),
+	}
+
+	userAPI.Get("/with_middleware", myMiddleware, versioning.NewMatcher(myVersions))
 
 	e := httptest.New(t, app)
 
@@ -44,32 +63,17 @@ func TestNewMatcher(t *testing.T) {
 	e.GET("/api/user").WithHeader(versioning.AcceptVersionHeaderKey, "2.9.9").Expect().
 		Status(iris.StatusOK).Body().Equal(v2Response)
 
+	// middleware as usual.
+	ex := e.GET("/api/user/with_middleware").WithHeader(versioning.AcceptVersionHeaderKey, "1.0").Expect()
+	ex.Status(iris.StatusOK).Body().Equal(v10Response)
+	ex.Header("X-Custom").Equal("something")
+
 	e.GET("/api/user").WithHeader(versioning.AcceptVersionHeaderKey, "3.0").Expect().
 		Status(iris.StatusNotFound).Body().Equal("Not Found")
 }
 
 func TestNewGroup(t *testing.T) {
 	app := iris.New()
-	// userAPI := app.Party("/api/user")
-
-	// userAPIV10 := versioning.NewGroup("1.0", userAPI)
-	// userAPIV10.Get("/", sendHandler(v10Response))
-	// userAPIV2 := versioning.NewGroup(">= 2, < 3", userAPI)
-	// userAPIV2.Get("/", sendHandler(v2Response))
-
-	// ---
-
-	// userAPI := app.Party("/api/user")
-	// userVAPI := versioning.NewGroup(userAPI)
-	// userAPIV10 := userVAPI.Version("1.0")
-	// userAPIV10.Get("/", sendHandler(v10Response))
-
-	// userAPIV10 := userVAPI.Version("2.0")
-	// userAPIV10.Get("/", sendHandler(v10Response))
-	// userVAPI.NotFound(...)
-	// userVAPI.Build()
-
-	// --
 
 	userAPI := app.Party("/api/user")
 	// [... static serving, middlewares and etc goes here].
