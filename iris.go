18ab198d447be2e7c60d75d076c0a21190f684eb
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-08-12 12:11:08 +0300

    version 11.2.5
    Former-commit-id: 1fc28c569cd8d45a0575afa342ae85b457ec6366

--- iris.go
+++ iris.go
@@ -37,7 +37,7 @@ import (
 
 var (
 	// Version is the current version number of the Iris Web Framework.
-	Version = "11.2.4"
+	Version = "11.2.5"
 )
 
 // HTTP status codes as registered with IANA.
