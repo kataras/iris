c935e5d7d111654fdfcda2971d5060e8b0bef03f
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2019-10-26 18:40:35 +0300

    add HISTORY entry for v12
    Former-commit-id: 778fedba71bc146709e840c7a37545e516c7a287

--- doc.go
+++ doc.go
@@ -42,9 +42,9 @@ Current Version
 
 Installation
 
-The only requirement is the Go Programming Language, at least version 1.12.
+The only requirement is the Go Programming Language, at least version 1.13.
 
-    $ go get github.com/kataras/iris@master
+    $ go get github.com/kataras/iris/v12@v12.0.0
 
 Wiki:
 
