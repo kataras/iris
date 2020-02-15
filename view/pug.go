fc59ff0fbd32dfab5807a7a57466abd0be9ea746
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-02-15 21:16:21 +0200

    fix of https://github.com/kataras/iris/issues/1450#issuecomment-586581677

--- view/pug.go
+++ view/pug.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io/ioutil"
 	"path"
+	"strings"
 
 	"github.com/iris-contrib/jade"
 )
@@ -22,28 +23,34 @@ import (
 // https://github.com/kataras/iris/tree/master/_examples/view/template_pug_3
 func Pug(directory, extension string) *HTMLEngine {
 	s := HTML(directory, extension)
+
 	s.middleware = func(name string, text []byte) (contents string, err error) {
+		name = path.Join(path.Clean(directory), name)
 		tmpl := jade.New(name)
-		// Fixes: https://github.com/kataras/iris/issues/1450
-		// by adding a custom ReadFunc inside the jade parser.
-		// And Also able to use relative paths on "extends" and "include" directives:
-		// e.g. instead of extends "templates/layout.pug" we use "layout.pug"
-		// so contents of templates are independent of their root location.
 		tmpl.ReadFunc = func(name string) ([]byte, error) {
-			name = path.Join(directory, name)
+			if !strings.HasPrefix(path.Clean(name), path.Clean(directory)) {
+				name = path.Join(directory, name)
+			}
+
 			if s.assetFn != nil {
 				return s.assetFn(name)
 			}
 			return ioutil.ReadFile(name)
 		}
 
-		tmpl, err = tmpl.Parse(text)
+		// Fixes: https://github.com/kataras/iris/issues/1450
+		// by adding a custom ReadFunc inside the jade parser.
+		// And Also able to use relative paths on "extends" and "include" directives:
+		// e.g. instead of extends "templates/layout.pug" we use "layout.pug"
+		// so contents of templates are independent of their root location.
+
+		exec, err := tmpl.Parse(text)
 		if err != nil {
 			return
 		}
 
 		b := new(bytes.Buffer)
-		tmpl.WriteIn(b)
+		exec.WriteIn(b)
 		return b.String(), nil
 	}
 	return s
