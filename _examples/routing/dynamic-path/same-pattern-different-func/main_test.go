65440ab772442d736128d2670abf20b672925bb6
Gerasimos (Makis) Maropoulos <kataras2006@hotmail.com>
2020-07-02 20:56:22 +0300

    add an example (and a test case) for same route path pattern but different macro function's input argument

---
+++ _examples/routing/dynamic-path/same-pattern-different-func/main_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/kataras/iris/v12/core/memstore"
+	"github.com/kataras/iris/v12/httptest"
+)
+
+func TestSameParameterTypeDifferentMacroFunctions(t *testing.T) {
+	app := newApp()
+	e := httptest.New(t, app)
+
+	type resp struct {
+		Handler string         `json:"handler"`
+		Params  memstore.Store `json:"params"`
+	}
+
+	var (
+		expectedIndex = resp{
+			Handler: "iris/_examples/routing/dynamic-path/same-pattern-different-func.handler1",
+			Params:  nil,
+		}
+		expectedHTMLPage = resp{
+			Handler: "iris/_examples/routing/dynamic-path/same-pattern-different-func.handler1",
+			Params: memstore.Store{
+				{Key: "page", ValueRaw: "random.html"},
+			},
+		}
+		expectedZipName = resp{
+			Handler: "iris/_examples/routing/dynamic-path/same-pattern-different-func.handler2",
+			Params: memstore.Store{
+				{Key: "name", ValueRaw: "random.zip"},
+			},
+		}
+	)
+
+	e.GET("/").Expect().Status(httptest.StatusOK).JSON().Equal(expectedIndex)
+	e.GET("/api/random.html").Expect().Status(httptest.StatusOK).JSON().Equal(expectedHTMLPage)
+	e.GET("/api/random.zip").Expect().Status(httptest.StatusOK).JSON().Equal(expectedZipName)
+}
