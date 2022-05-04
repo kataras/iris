98518d8e20f9685e862bd633f0ea844d0518a749
kataras <kataras2006@hotmail.com>
2022-05-05 02:49:03 +0300

    minor

--- x/errors/path_type_parameter_error_handler.go
+++ x/errors/path_type_parameter_error_handler.go
@@ -1,10 +1,13 @@
 package errors
 
-import "github.com/kataras/iris/v12/context"
+import (
+	"github.com/kataras/iris/v12/context"
+	"github.com/kataras/iris/v12/macro/handler"
+)
 
 // DefaultPathTypeParameterErrorHandler registers an error handler for macro path type parameter.
 // Register it with Application.Macros().SetErrorHandler(DefaultPathTypeParameterErrorHandler).
-func DefaultPathTypeParameterErrorHandler(ctx *context.Context, paramIndex int, err error) {
+var DefaultPathTypeParameterErrorHandler handler.ParamErrorHandler = func(ctx *context.Context, paramIndex int, err error) {
 	param := ctx.Params().GetEntryAt(paramIndex) // key, value fields.
 	InvalidArgument.DataWithDetails(ctx, "invalid path parameter", err.Error(), param)
 }
